a791813b0bb5071879e79ad398a5202aeb3e1956
arthik45 <arthiksk64@gmail.com>
2024-09-25 15:18:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function division(){
+  let num1=document.getElementById('first').value;
+  let num2=document.getElementById('second').value;
+
+  document.getElementById('demo').innerHTML=num1/num2;
+}
+
+function multiply(){
+  let num1=document.getElementById('first').value;
+  let num2=document.getElementById('second').value;
+
+  document.getElementById('demo').innerHTML=num1*num2;
+}
